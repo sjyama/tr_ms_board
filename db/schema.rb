20fd213b436c28efa66622a26a8c0cfba1049bed
sjyama_mbp <grinpeace101@gmail.com>
2019-05-04 10:36:43 +0900

    titleカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_004114) do
+ActiveRecord::Schema.define(version: 2019_05_04_002340) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
